f20717991de9c614fe9ffc758fecd12a49a2ed04
wty13 <wetyamamoto@gmail.com>
2021-08-27 18:15:24 -0300

    first commit

---
+++ Ciclo 2/Aula/aulaJS5/script.js
@@ -0,0 +1,29 @@
+import{msgLog} from './modulos/modulos.js'
+
+window.onload=function(){
+//msgLog("Olá");
+/*var data = new Date();
+
+semana = ["dom","seg","ter","quart","qui","sex"]
+
+console.log(semana[data.getDay()]);
+var dta = data.toLocaleString('pt-BR',{year: 'numeric', month:'2-digit', day:'2-digit'});
+console.log(dta);*/
+
+/*var n='2';
+
+try{
+	n = Number(n);
+	if(isNaN(n)) throw "Este valor não é um número";
+
+} catch(erro){
+	console.log(`Erro: ${erro}`)
+} finally {
+	console.log("Terminado");
+}*/
+
+
+
+
+
+}
